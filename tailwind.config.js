04fb8ac2228327668fb49cc876a05243d230a059
Njmilon <njmilon1@gmail.com>
2022-11-23 22:02:17 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#149777",
+          secondary: "#0074BA",
+          accent: "#FFC800",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
+
