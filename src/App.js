baeefdbe62f40fa8ea48b1ea1235fc5030222120
Njmilon <njmilon1@gmail.com>
2022-11-23 23:08:06 +0600

    router & layout added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-      <button className='btn btn-primary'>primary</button>
-      <button className='btn btn-secondary'>secondary</button>
-      <button className='btn btn-accent'>accent</button>
-      <button className='btn btn-neutral'>hello btn</button>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
