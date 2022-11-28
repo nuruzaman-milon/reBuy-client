2675fad9d8bd9c3d865c6649cf965052a08fcc98
Njmilon <njmilon1@gmail.com>
2022-11-28 14:18:16 +0600

    seller add product & my product added

--- src/pages/DashBoard/AddProduct/AddProduct.js
+++ src/pages/DashBoard/AddProduct/AddProduct.js
@@ -1,9 +1,131 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const AddProduct = () => {
+    const {user} = useContext(AuthContext);
+    const navigate = useNavigate();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        fetch('http://localhost:5000/products', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                navigate('/dashboard/myProduct')
+                toast.success('product added successfully')
+            })
+    };
+
+    //load all category data
+    const { isLoading, error, data: categories } = useQuery({
+        queryKey: ['category'],
+        queryFn: () =>
+            fetch('http://localhost:5000/category').then(res =>
+                res.json()
+            )
+    })
+
+    if (isLoading) return 'Loading...';
+
+    if (error) return 'An error has occurred: ' + error.message;
+    // console.log(categories[0]);
+
+    const date = new Date();
     return (
         <div>
-            <h1>add product here</h1>
+            <form className='w-[80%] mx-auto' onSubmit={handleSubmit(onSubmit)}>
+                <h1 className='text-center my-4 text-2xl font-bold'>Add Your Product</h1>
+                <div className="mt-4">
+                    <div className="mt-4">
+                        <p className='text-center text-red-600'></p>
+                        <label className="block" htmlFor="name">Product Name</label>
+                        <input required type="text" {...register("name", { required: true })} placeholder="Product Name"
+                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                    </div>
+                    <div className="mt-4">
+                        <p className='text-center text-red-600'></p>
+                        <label className="block" htmlFor="price">Original Price</label>
+                        <input required type="text" {...register("originalPrice", { required: true })} placeholder="Product Original Price"
+                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                    </div>
+                    <div className="mt-4">
+                        <p className='text-center text-red-600'></p>
+                        <label className="block" htmlFor="price">Resale Price</label>
+                        <input required type="text" {...register("resalePrice", { required: true })} placeholder="Product Resale Price"
+                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                    </div>
+                    <div className="mt-4">
+                        <label className="block">Location</label>
+                        <input required type="text" {...register("location", { required: true })} placeholder="Location"
+                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                    </div>
+                    <div className="mt-4">
+                        <label className="block">Years Of Use</label>
+                        <input required type="text" {...register("yearsOfUse", { required: true })} placeholder="Years Of Use"
+                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                    </div>
+                    <div className="mt-4">
+                        <label className="block">Product Photo Url</label>
+                        <input required type="text" {...register("img", { required: true })} placeholder="Img Url"
+                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                    </div>
+                    <div className="mt-4">
+                        <input value={date} hidden required type="text" {...register("time", { required: true })}
+                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                    </div>
+
+                    <div className="mt-4">
+                        <input value={user?.email} hidden required type="text" {...register("sellerEmail", { required: true })}
+                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                    </div>
+
+                    <div className="mt-4">
+                        <label className="block">Seller Name</label>
+                        <input required type="text" {...register("sellerName", { required: true })}
+                            placeholder="Seller Name"
+                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                    </div>
+                    <div className="mt-4">
+                        <label className="block">Seller Phone Number</label>
+                        <input required type="text" {...register("phoneNumber", { required: true })}
+                            placeholder="Seller Phone Number"
+                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600" />
+                    </div>
+
+
+                    <div className="mt-4">
+                        <label className="block mb-2">Select Product Condition</label>
+                        <select {...register("condition")} className="select select-bordered w-full">
+                            <option value="excellent" selected>Excellent</option>
+                            <option value="good">Good</option>
+                            <option value="fair">Fair</option>
+                        </select>
+                    </div>
+                    {/* categoy */}
+
+                    <div className="mt-4">
+                        <label className="block mb-2">Select Category</label>
+                        <select {...register("category_id")} className="select select-bordered w-full">
+                            {
+                                categories.map(category => <option category={category} value={category?._id}>{category?.title}</option>)
+                            }
+                        </select>
+                    </div>
+
+                    <div className="flex">
+                        <button className="w-full my-4 text-white btn btn-sm btn-primary p-1">Add Product</button>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
--- src/pages/DashBoard/MyProduct/MyProduct.js
+++ src/pages/DashBoard/MyProduct/MyProduct.js
@@ -1,9 +1,82 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const MyProduct = () => {
+    const { user } = useContext(AuthContext);
+    const uri = `http://localhost:5000/products?email=${user?.email}`;
+
+    const { isLoading, error, data: products } = useQuery({
+        queryKey: ['products', user?.email],
+        queryFn: async () =>
+            {
+                const res = await fetch(uri, {
+                    headers:{
+                        authorization:`bearer ${localStorage.getItem('accessToken')}`
+                    }
+                });
+                const data = await res.json();
+                return data;
+            } 
+    })
+
+    if (isLoading) return 'Loading...';
+    if (error) return 'An error has occurred: ' + error.message;
+
     return (
         <div>
-            <h1>my product here</h1>
+            {
+                products.length > 0 ?
+                <div className="overflow-x-auto w-full">
+                    <table className="table w-full">
+                        {/* <!-- head --> */}
+                        <thead>
+                            <tr>
+                                <th>Sl</th>
+                                <th>Product Info</th>
+                                <th>Seller Phone</th>
+                                <th>Seller Location</th>
+                                <th>Status</th>
+                                <th>Advertise</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {
+                                products.map((product, i) => <tr>
+                                    <td>
+                                        {i + 1}
+                                    </td>
+                                    <td>
+                                        <div className="flex items-center space-x-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={product?.img} alt="booking product images" />
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div className="font-bold">{product?.name}</div>
+                                                <div className="text-sm opacity-50">৳{product?.resalePrice}</div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>{product?.phoneNumber}</td>
+                                    <td>{product?.location}</td>
+                                    <td>unsold</td>
+                                    <td><button className='btn btn-primary btn-sm'>Advertise</button></td>
+                                    <td><button className='btn btn-primary btn-sm'>Delete</button> </td>
+                                </tr>)
+                            }
+
+
+                        </tbody>
+
+                    </table>
+                </div>
+                :
+                <h1 className='ml-40 mt-6 text-2xl font-bold'>You have no booking! Please add a booking! </h1>
+            }
         </div>
     );
 };
